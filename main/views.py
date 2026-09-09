6f5755d075bb4ec18688ab064ad8dedc5b37faee
ImmanuelMarvinPandjaitan <burhan@example.com>
2026-09-09 15:40:21 +0700

    Tutorial 02

---
+++ main/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+from django.shortcuts import render
+
+from main.models import Experience
+
+
+def show_main(request):
+    context = {
+        "name": "Immanuel Marvin Pandjaiatan",
+        "npm": "2506623881",
+        "study_program": "S1 Ilmu Komputer",
+        "bio": (
+            "Mahasiswa Ilmu Komputer Universitas Indonesia yang tertarik "
+            "pada pengembangan perangkat lunak dan pendidikan."
+        ),
+    }
+    return render(request, "index.html", context)
+
+
+def show_experience(request):
+    context = {
+        "name": "Burhan",
+        "experience_list": Experience.objects.all(),
+    }
+    return render(request, "experience.html", context)
+# Create your views here.
